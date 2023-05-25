87c0959e928a7b52b69e6d302a7589ff34b8c844
your-highness <hiob@noisecore.org>
2023-05-25 13:11:24 +0200

    Introduce Error Handling for createChatCompletion() thread
    Closes #35

--- src/openai-thread-completion.js
+++ src/openai-thread-completion.js
@@ -1,3 +1,6 @@
+const { Log } = require('debug-level')
+const log = new Log('bot')
+
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     apiKey: process.env["OPENAI_API_KEY"]
@@ -8,12 +11,21 @@ const model = process.env["OPENAI_MODEL_NAME"] ?? 'gpt-3.5-turbo'
 const max_tokens = Number(process.env["OPENAI_MAX_TOKENS"] ?? 2000)
 
 async function continueThread(messages) {
-    const response = await openai.createChatCompletion({
-        messages: messages,
-        model,
-        max_tokens
-    });
-    return response.data?.choices?.[0]?.message?.content
+  try {
+      const response = await openai.createChatCompletion({
+          messages: messages,
+          model,
+          max_tokens
+      });
+      log.trace(response.data?.choices?.[0]?.text);
+      return response.data?.choices?.[0]?.message?.content
+  } catch (e) {
+      if (e.response) {
+        log.error(e.response.status);
+        log.error(e.response.data);
+      }
+      throw e;
+  }
 }
 
 module.exports = { continueThread }
